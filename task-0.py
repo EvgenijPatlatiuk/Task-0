45a57d09da0d7bbea37ec793116927872b043165
EvgenijPatlatiuk <evgenijpatlatuk@gmail.com>
2021-07-05 16:28:55 +0300

    создал программу для рандомных чисел
    1-Сформувати список з 30 випадкових цілих чисел від -100 до + 100.
    Знайти максимальний елемент списку і його порядковий номер. Отримати
    інший список, що складається тільки з непарних чисел вихідного списку
    або повідомити, що таких чисел немає. Отриманий список вивести в
    порядку зменшення елементів.

---
+++ task-0.py
@@ -0,0 +1,15 @@
+import random
+random_list = [random.randrange(-100,100) for i in range(30)]
+print("\n Random list of numbers from -100 to 100 is: ", random_list)
+max_element = max(random_list)
+position = random_list.index(max_element)
+print("\nA maximum number from the list is: ", max_element, "\nin position: ", position + 1)
+new_list=[]
+for element in random_list:
+    if (element % 2) == 1:
+        new_list.append(element)
+if len(new_list) == 0:
+  print("\nthere isn't any of odd numbers")
+else:
+  new_list.sort(reverse=True)
+  print("\nThe new list is: ", new_list)
